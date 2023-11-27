e2a29245d1fb683e33d9b96a92e4f69eb8a33f63
kkb0318 <smallobuti@gmail.com>
2023-11-27 20:55:04 +0900

    ld d,s

--- gb-emu/src/cpu/instructions.rs
+++ gb-emu/src/cpu/instructions.rs
@@ -1,10 +1,51 @@
+use super::operand::{IO16, IO8};
 use super::Cpu;
 use crate::peripherals::Peripherals;
+use std::sync::atomic::{AtomicU16, AtomicU8, Ordering::Relaxed};
 
 impl Cpu {
     /// no operation  何もせず次の命令をfetchするだけ
     pub fn nop(&mut self, bus: &mut Peripherals) {
         self.fetch(bus)
+    }
+
+    // sの値をdに格納する (8 bit)
+    pub fn ld<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where
+        Self: IO8<D> + IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+              VAL8.store(v, Relaxed);
+              go!(1);
+            },
+            1: if self.write8(bus, dst, VAL8.load(Relaxed)).is_some() {
+              go!(2);
+             },
+            2: {
+              go!(0);
+              self.fetch(bus);
+            },
+        });
+    }
+    // sの値をdに格納する (16 bit)
+    pub fn ld16<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where
+        Self: IO16<D> + IO16<S>,
+    {
+        step!((), {
+        0: if let Some(v) = self.read16(bus, src) {
+          VAL16.store(v, Relaxed);
+          go!(1);
+        },
+        1: if self.write16(bus, dst, VAL16.load(Relaxed)).is_some() {
+          go!(2);
+        },
+        2: {
+          go!(0);
+          self.fetch(bus);
+          },
+        });
     }
 }
 
