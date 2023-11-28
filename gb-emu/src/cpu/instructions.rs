88ff7c827b0c56986495cb0b3197c7f1e58c3ab4
kkb0318 <smallobuti@gmail.com>
2023-11-28 23:34:47 +0900

    cp命令

--- gb-emu/src/cpu/instructions.rs
+++ gb-emu/src/cpu/instructions.rs
@@ -9,7 +9,11 @@ impl Cpu {
         self.fetch(bus)
     }
 
-    // sの値をdに格納する (8 bit)
+    /// LD命令
+    /// メモリやレジスタ間でのデータ転送
+    /// sの値をdに格納する (8 bit)
+    /// where句により、SelfがD, Sに対するIO16を実装しているというトレイト境界が設定されている
+    /// これにより、D, SはReg8, Imm8 といった、IO8が実装されている型のみ許容される
     pub fn ld<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
     where
         Self: IO8<D> + IO8<S>,
@@ -28,7 +32,8 @@ impl Cpu {
             },
         });
     }
-    // sの値をdに格納する (16 bit)
+    /// メモリやレジスタ間でのデータ転送
+    /// sの値をdに格納する (16 bit)
     pub fn ld16<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
     where
         Self: IO16<D> + IO16<S>,
@@ -46,6 +51,26 @@ impl Cpu {
           self.fetch(bus);
           },
         });
+    }
+    /// CP命令
+    /// A レジスタとs の値の比較
+    /// （A レジスタからsの値を引き フラグレジスタを適切に設定. 演算結果は格納されない
+    /// Z 演算結果が0 の場合は1 にする
+    /// N 無条件に1 にする
+    /// H 4 bit 目からの繰り下がりが発生した場合は1 にする
+    /// C 8 bit 目からの繰り下がりが発生した場合は1 にする
+    pub fn cp<S: Copy>(&mut self, bus: &Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            let (result, carry) = self.regs.a.overflowing_sub(v);
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(true);
+            self.regs.set_hf((self.regs.a & 0xf) < (v & 0xf));
+            self.regs.set_cf(carry);
+            self.fetch(bus);
+        }
     }
 }
 
